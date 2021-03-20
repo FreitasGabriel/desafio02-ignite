7656c40c79d4d3c39ad6e974e86ff1b13dfb78c5
Gabriel Freitas <gabriel.freitas@bemobi.com.br>
2021-03-20 20:29:49 -0300

    finish challenge

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,26 @@
+import { useMovieContext } from '../hooks/useMovieContext'
+import { Button } from '../components/Button';
+
 export function SideBar() {
-  // Complete aqui
+
+  const { handleClickButton, genres, selectedGenreId } = useMovieContext()
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
